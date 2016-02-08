f13d018d61f7f3c87d532fe63e8c2d4539b967e9
oscar <oscar@oscar-PC>
2016-02-08 23:04:40 +0100

    Merge origin/master
    Conflicts:
    	ejemploTablas/src/main/java/com/ejemplotablas/JFrameDatos.java

--- mavenJavaFXAPP/src/main/java/com/mavenjavafxapp/FXMLController.java
+++ mavenJavaFXAPP/src/main/java/com/mavenjavafxapp/FXMLController.java
@@ -5,36 +5,44 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private ComboBox<String> combo;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("title");
+        alert.setHeaderText("Information Alert");
+        String s = "This is an example of JavaFX 8 Dialogs... ";
+        alert.setContentText(s);
+        alert.show();
     }
-    
+
     @FXML
     private void handleCombo(ActionEvent event) {
         System.out.println(combo.getSelectionModel().getSelectedItem());
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         combo.getItems().addAll(
-    "Option 4",
-    "Option 5",
-    "Option 6"
-);
-    }    
+                "Option 4",
+                "Option 5",
+                "Option 6"
+        );
+    }
 }
