b8178ff480ed44d30a838552d54f4c5291c3b316
oscar <oscar@oscar-PC>
2016-02-26 00:36:56 +0100

    Merge origin/master
    Conflicts:
    	ejemploTablas/src/main/java/com/ejemplotablas/JFrameDatos.java

--- ejemploTablas/src/main/java/dao/DBConnection.java
+++ ejemploTablas/src/main/java/dao/DBConnection.java
@@ -1,9 +1,11 @@
+package dao;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao;
+
 
 import config.Configuration;
 import java.sql.Connection;
